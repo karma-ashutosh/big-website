dd751d65f167d031fd85836e3b3f348ee8778c39
Ashutosh <karmav44990@gmail.com>
2024-12-05 15:37:30 +0530

    added background scrolling image

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,42 @@ import {ImageCarousel} from "@/components/carousel-row.tsx";
 import Footer from "@/components/footer.tsx";
 import GujratBanner from "@/components/GujratBanner.tsx";
 
+import { FC, ReactNode } from "react";
+
+interface ParallaxBackgroundProps {
+    backgroundUrl: string;
+    children: ReactNode;
+}
+
+const ParallaxBackground: FC<ParallaxBackgroundProps> = ({ backgroundUrl, children }) => {
+    return (
+        <div className="relative overflow-hidden">
+            {/* Background */}
+            <div
+                className="absolute top-0 left-0 w-full h-full z-15 bg-cover bg-center"
+                style={{
+                    backgroundImage: `url(${backgroundUrl})`,
+                    backgroundAttachment: "fixed",
+                }}
+            />
+            {/* Foreground Content */}
+            <div className="relative z-10">
+                {children}
+            </div>
+        </div>
+    );
+};
+
 function App() {
   return (
     <>
-      <div className="bg-footer">
+      <div className="bg-footer relative">
           <Banner />
-          <ImageGrid/>
-          <GujratBanner />
-          <ImageCarousel/>
+          <ParallaxBackground backgroundUrl="https://sahjeevan.org/wp-content/uploads/2023/03/map-marker-1.png">
+              <ImageGrid/>
+              <GujratBanner />
+              <ImageCarousel/>
+          </ParallaxBackground>
           <Footer />
       </div>
     </>
