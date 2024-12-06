c732a84133998b3a564e8b22145e906f0e2eabd1
Ashutosh <karmav44990@gmail.com>
2024-12-06 14:06:54 +0530

    fixed vercel issue

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,3 +1,4 @@
+import tailwindcssAnimate from 'tailwindcss-animate'
 /** @type {import('tailwindcss').Config} */
 export default {
     darkMode: ["class"],
@@ -55,7 +56,7 @@ export default {
   		}
   	}
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [tailwindcssAnimate],
 }
 
 // module.exports = {
